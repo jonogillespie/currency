d67b4366ba4a69dd9baa5571478705e2a7de9656
jonathangillespie <jgillespie@ropeiq.co.uk>
2021-04-25 18:45:53 +0100

    Refactor main.js

--- Presentation/UI/js/main.js
+++ Presentation/UI/js/main.js
@@ -1,41 +1,74 @@
 $(document).ready(() => {
-  loadData();
+
+  loadData()
   
   setInterval(function(){
-    loadData() 
-  }, 3000);
-});
+
+    loadData()
+
+  }, 3000)
+
+})
 
 const loadData = () => {
+
   const settings = {
-    url: `http://localhost:9080/v1/currencies`,
+    url: `${baseUrl}/v1/currencies`,
     method: "GET",
     timeout: 0
-  };
+  }
 
   $.ajax(settings).done((response) => {
 
-    let html = '';
-    response.data.forEach(element => {
-      html += buildRow(element);
-    });
+    let html = buildAllRowsHtml(response)
 
-    const currencyTableRow = $('#currency-table tr');
-    currencyTableRow.not(':first').not(':last').remove()
-    currencyTableRow.first().after(html);
-    $('#error-message').hide();
+    addRowsToTable(html)
+
+    hideErrorMessage()
+
   }).fail(() => {
-    $('#error-message').show();
-  });
+
+    showErrorMessage()
+  })
 }
 
-const buildRow = (element) => {
-  return '<tr>' +
+const buildAllRowsHtml = (response) => {
+
+  let html = ''
+
+  response.data.forEach(element => {
+    html += buildRow(element)
+  })
+
+  return html
+}
+
+const buildRow = (element) =>
+    '<tr>' +
     '         <td>' + element.abbreviation + '</td>' +
     '         <td>' + element["15m"] + '</td>' +
     '         <td>' + element.last + '</td>' +
     '         <td>' + element.buy + '</td>' +
     '         <td>' + element.sell + '</td>' +
     '         <td>' + element.symbol + '</td>' +
-    '      </tr>';
+    '      </tr>'
+
+const addRowsToTable = (html) => {
+
+  const currencyTableRow = $('#currency-table tr')
+
+  removeCurrentTableRows(currencyTableRow)
+
+  currencyTableRow.first().after(html)
 }
+
+const removeCurrentTableRows = (currencyTableRow) =>
+    currencyTableRow.not(':first').not(':last').remove()
+
+const hideErrorMessage = () =>
+    $('#error-message').hide()
+
+const showErrorMessage = () =>
+    $('#error-message').show()
+
+const baseUrl = 'http://localhost:9080'
